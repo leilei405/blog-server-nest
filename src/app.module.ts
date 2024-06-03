997782e8ac9feff438f7175ace88dc24cfea2020
leilei405 <1601178425@qq.com>
2024-06-03 14:13:18 +0800

    feat: 使用type ORM 链接MySql数据库

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config'
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BlogModule } from './blog/blog.module';
 
 @Module({
-  imports: [BlogModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.MYSQL_HOST,
+      port: parseInt(process.env.MYSQL_PORT),
+      username: process.env.MYSQL_USER,
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.MYSQL_DB,
+      entities: [],
+      synchronize: true, // 生产环境建议关闭，否则数据表结构会随着代码变化而改变
+    }),
+    BlogModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
