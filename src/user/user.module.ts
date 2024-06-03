154207566406e1a1d5163dab2a3c15740a01825c
leilei405 <1601178425@qq.com>
2024-06-03 20:54:02 +0800

    feat: 使用Jwt 做登录校验，并返回token

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,9 +3,19 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { User } from './user.entity'
+import { JwtModule } from '@nestjs/jwt'
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    JwtModule.register({
+      global: true,
+      secret: '123456',
+      signOptions: { 
+        expiresIn: 3600 
+      },
+    }),
+  ],
   controllers: [UserController],
   providers: [UserService]
 })
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -3,11 +3,14 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Like, Repository } from 'typeorm';
 import { User } from './user.entity'
 import { LoginUserDTO } from './dto/loginuser.dto'
+import { JwtService } from '@nestjs/jwt'
+
 @Injectable()
 export class UserService {
     constructor(
         @InjectRepository(User) // 注入User实体类模块
-        private readonly userRepository: Repository<User>
+        private readonly userRepository: Repository<User>, 
+        private readonly jwtService: JwtService
     ) {}
 
     async findOne(loginUserDTO: LoginUserDTO) {
@@ -16,7 +19,13 @@ export class UserService {
         if (data == null) {
             throw new HttpException('用户名或密码错误', HttpStatus.BAD_REQUEST)
         }
-        const payLoad = { username, id: data.id, realname: data.realname }
-        return payLoad;
+        const payLoad = { 
+            username,
+            realname: data.realname,
+            id: data.id,
+        }
+
+        return await this.jwtService.signAsync(payLoad)
+        // return payLoad;
     }
 }
