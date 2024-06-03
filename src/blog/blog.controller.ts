3b2ea8bd75d1e36ca2edf2a1d3450c9d1a995832
leilei405 <1601178425@qq.com>
2024-06-03 19:41:32 +0800

    feat: 开发blog所有功能

--- src/blog/blog.controller.ts
+++ src/blog/blog.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, Query } from '@nestjs/common';
 import { CreateBlogDto } from './dto/create.dto';
 import { BlogService } from './blog.service';
 
@@ -9,45 +9,36 @@ export class BlogController {
 
     constructor (private readonly blogService: BlogService ) {}
 
+    // 查询博客
     @Get()
-    async findAll( @Query('name') name: String, @Query('age') age: Number) {
-       console.log(name, age, '获取列表数据');
-        return {data: 'this is blog', msg: '获取成功'};
+    async findAll(@Query('keyword') keyword: string, @Query('author') author: string) {
+        return await this.blogService.findAll(author, keyword);
     }
 
     // 动态路由的参数 param
     @Get(":id")
     async findOne(@Param('id') id: string) {
        const data = await this.blogService.findOne(+id);
-       console.log(data);
-       
        return data;
     }
 
-    // 删除
+    // 删除博客
     @Delete(":id")
     async remove(@Param('id') id: string) {
-        console.log(id);
-        return { id };
+        return await this.blogService.remove(+id);
     }
 
-    // 创建
-    @Post() // @Body 收集请求体内容
+    // 创建博客
+    @Post()
     async create(@Body() createBlogDto: CreateBlogDto) {
         createBlogDto.author = 'admin';
-       console.log(createBlogDto);
-       
         const data = await this.blogService.create(createBlogDto);
         return data;
     }
 
-    // 更新
-    @Patch(":id") // @Body 收集请求体内容
-    async update(
-        @Param("id") id: string,
-        @Body() createBlogDto: CreateBlogDto
-    ) {
-        console.log(id, createBlogDto);
-        return { data: true, msg: '更新成功' };
+    // 更新博客
+    @Patch(":id")
+    async update(@Param("id", ParseIntPipe) id: number,@Body() createBlogDto: CreateBlogDto) {
+        return await this.blogService.update(id, createBlogDto);
     }
 }
--- src/blog/blog.service.ts
+++ src/blog/blog.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { Blog } from './blog.entity'
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { Like, Repository } from 'typeorm';
 import { CreateBlogDto } from './dto/create.dto';
 
 @Injectable()
@@ -11,11 +11,38 @@ export class BlogService {
         private readonly blogRepository: Repository<Blog>
     ) {}
 
+    // 查询所有博客
+    async findAll(author: string = '', keyword: string = '') {
+        const condition = {}
+        if (author) {
+            condition['author'] = author;
+        }
+        if (keyword) {
+            condition['title'] = Like(`%${keyword}%`);
+        }
+        return await this.blogRepository.find({
+            where: condition,
+            order: { createAt: 'DESC' }
+        });
+    }
+
+    // 查询单个博客
     async findOne(id: number) {
         return await this.blogRepository.findOneBy({ id });
     }
 
+    // 创建博客
     async create(blog: CreateBlogDto) {
         return await this.blogRepository.save(blog);
+    }
+
+    // 更新博客
+    async update(id: number, blog: CreateBlogDto) {
+        return await this.blogRepository.update(id , blog);
+    }
+
+    // 删除博客
+    async remove(id: number) {
+        return await this.blogRepository.delete(id);
     }
 }
